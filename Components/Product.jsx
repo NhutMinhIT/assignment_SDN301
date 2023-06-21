ed84c51c4912689d78570d3fdd8438322f692c73
FPTnhutminh00 <nhutminh.fpt@gmail.com>
2023-06-21 14:47:20 +0700

    Done Reload with State

--- Components/Product.jsx
+++ Components/Product.jsx
@@ -10,11 +10,31 @@ const Product = () => {
     const navigation = useNavigation();
 
     const [filteredCategory, setFilteredCategory] = useState([]);
-    const [flowerAlbum, setFlowerAlbum] = useState(flowers);
+    const [flowerAlbum, setFlowerAlbum] = useState([]);
     const [selectedCategory, setSelectedCategory] = useState('All');
 
+    const loadFlowers = async () => {
+        try {
+            const storedFlowers = await AsyncStorage.getItem('flowers');
+            if (storedFlowers) {
+                const parsedFlowers = JSON.parse(storedFlowers);
+                setFlowerAlbum(parsedFlowers);
+                applyFilter(parsedFlowers, selectedCategory);
+            } else {
+                setFlowerAlbum(flowers);
+                applyFilter(flowers, selectedCategory);
+            }
+        } catch (error) {
+            console.log('Error retrieving flowers from AsyncStorage:', error);
+        }
+    };
+
     useEffect(() => {
-        const saveFlowerStore = async () => {
+        loadFlowers();
+    }, []);
+
+    useEffect(() => {
+        const saveFlowers = async () => {
             try {
                 await AsyncStorage.setItem('flowers', JSON.stringify(flowerAlbum));
             } catch (error) {
@@ -22,50 +42,23 @@ const Product = () => {
             }
         };
 
-        saveFlowerStore();
+        saveFlowers();
     }, [flowerAlbum]);
 
-    useEffect(() => {
-        const getFlowerStore = async () => {
-            try {
-                const storeFlowers = await AsyncStorage.getItem('flowers');
-                if (storeFlowers) {
-                    setFlowerAlbum(JSON.parse(storeFlowers));
-                } else {
-                    setFlowerAlbum(flowers);
-                }
-            } catch (error) {
-                console.log('Error retrieving flowers from AsyncStorage:', error);
-            }
-        };
-
-        getFlowerStore();
-    }, []);
-
-    useEffect(() => {
-        const applyFilter = () => {
-            let filteredFlowers = flowerAlbum;
-            if (selectedCategory !== 'All') {
-                filteredFlowers = flowerAlbum.filter((flower) => flower.category === selectedCategory);
-            }
-            setFilteredCategory(filteredFlowers);
-        };
-
-        applyFilter();
-    }, [flowerAlbum, selectedCategory]);
+    const applyFilter = (flowers, category) => {
+        let filteredFlowers = flowers;
+        if (category !== 'All') {
+            filteredFlowers = flowers.filter((flower) => flower.category === category);
+        }
+        setFilteredCategory(filteredFlowers);
+    };
 
     const handleFilterCategory = (category) => {
         setSelectedCategory(category);
+        applyFilter(flowerAlbum, category);
     };
 
     const handleFavourite = (flower) => {
-        const updatedFilteredCategory = filteredCategory.map((item) => {
-            if (item.id === flower.id) {
-                return { ...item, favourite: !item.favourite };
-            }
-            return item;
-        });
-
         const updatedFlowerAlbum = flowerAlbum.map((item) => {
             if (item.id === flower.id) {
                 return { ...item, favourite: !item.favourite };
@@ -73,33 +66,37 @@ const Product = () => {
             return item;
         });
 
-        setFilteredCategory(updatedFilteredCategory);
         setFlowerAlbum(updatedFlowerAlbum);
+        applyFilter(updatedFlowerAlbum, selectedCategory)
     };
 
     return (
         <ScrollView>
             <FilterCategory onCheckFilter={handleFilterCategory} />
             <View style={styles.container}>
-                {filteredCategory.map((flower) => (
-                    <TouchableOpacity
-                        style={styles.card}
-                        key={flower.id}
-                        onPress={() => {
-                            navigation.navigate('Detail', { flowerDetail: flower });
-                        }}
-                    >
-                        <Pressable style={styles.iconContainer} onPress={() => handleFavourite(flower)}>
-                            {flower.favourite === true ? (
-                                <Ionicons name="heart" size={24} color="#fa1905" />
-                            ) : (
-                                <Ionicons name="heart-outline" size={24} color="black" />
-                            )}
-                        </Pressable>
-                        <Image style={styles.image} source={flower.image} />
-                        <Text style={styles.name}>{flower.name}</Text>
-                    </TouchableOpacity>
-                ))}
+                {filteredCategory.length > 0 ? (
+                    filteredCategory.map((flower) => (
+                        <TouchableOpacity
+                            style={styles.card}
+                            key={flower.id}
+                            onPress={() => {
+                                navigation.navigate('Detail', { flowerDetail: flower });
+                            }}
+                        >
+                            <Pressable style={styles.iconContainer} onPress={() => handleFavourite(flower)}>
+                                {flower.favourite === true ? (
+                                    <Ionicons name="heart" size={24} color="#fa1905" />
+                                ) : (
+                                    <Ionicons name="heart-outline" size={24} color="black" />
+                                )}
+                            </Pressable>
+                            <Image style={styles.image} source={flower.image} />
+                            <Text style={styles.name}>{flower.name}</Text>
+                        </TouchableOpacity>
+                    ))
+                ) : (
+                    <Text>No flowers found.</Text>
+                )}
             </View>
         </ScrollView>
     );
